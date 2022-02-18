ee723dde9d38f6b35ceb1b07182968f3eade7a7a
Carlos Sánchez <cagudeloa@uninorte.edu.co>
2022-02-18 09:41:38 -0500

    Fix typo

--- app/src/main/java/com/casa98/currencies/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/casa98/currencies/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -23,7 +23,7 @@ class GetCoinUseCase @Inject constructor(
         } catch (e: HttpException) {
             emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred"))
         } catch (e: IOException) {
-            emit(Resource.Error(message = "Couldn't reach server, check your internet conection"))
+            emit(Resource.Error(message = "Couldn't reach server, check your internet connection"))
         }
     }
 }
--- app/src/main/java/com/casa98/currencies/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/casa98/currencies/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -21,7 +21,7 @@ class GetCoinsUseCase @Inject constructor(
         } catch (e: HttpException) {
             emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred"))
         } catch (e: IOException) {
-            emit(Resource.Error(message = "Couldn't reach server, check your internet conection"))
+            emit(Resource.Error(message = "Couldn't reach server, check your internet connection"))
         }
     }
 }
