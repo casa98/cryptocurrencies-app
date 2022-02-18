7c01c1c0084c0043feedeb50c2dd02f5e7b8418e
Carlos Sánchez <cagudeloa@uninorte.edu.co>
2022-02-18 15:34:45 -0500

    Fix (hope) uses cases. add INTERNET permission

--- app/src/main/java/com/casa98/currencies/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/casa98/currencies/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -2,9 +2,7 @@ package com.casa98.currencies.domain.use_case.get_coin
 
 import com.casa98.currencies.common.Resource
 import com.casa98.currencies.data.remote.dto.toCoinDetail
-import com.casa98.currencies.domain.model.CoinDetail
 import com.casa98.currencies.domain.respository.CoinRepository
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
@@ -13,15 +11,15 @@ import javax.inject.Inject
 class GetCoinUseCase @Inject constructor(
     private val repository: CoinRepository
 ) {
-    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+    operator fun invoke(coinId: String) = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading(data = null ))
             val coin = repository.getCoin(coinId).toCoinDetail()
             emit(Resource.Success(coin))
         } catch (e: HttpException) {
-            emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred"))
+            emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred", data = null))
         } catch (e: IOException) {
-            emit(Resource.Error(message = "Couldn't reach server, check your internet connection"))
+            emit(Resource.Error(message = "Couldn't reach server, check your internet connection", data = null))
         }
     }
 }
--- app/src/main/java/com/casa98/currencies/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/casa98/currencies/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -2,9 +2,7 @@ package com.casa98.currencies.domain.use_case.get_coins
 
 import com.casa98.currencies.common.Resource
 import com.casa98.currencies.data.remote.dto.toCoin
-import com.casa98.currencies.domain.model.Coin
 import com.casa98.currencies.domain.respository.CoinRepository
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
@@ -13,15 +11,15 @@ import javax.inject.Inject
 class GetCoinsUseCase @Inject constructor(
     private val repository: CoinRepository
 ) {
-    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+    operator fun invoke() = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading(data = null))
             val coins = repository.getCoins().map { it.toCoin() }
             emit(Resource.Success(coins))
         } catch (e: HttpException) {
-            emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred"))
+            emit(Resource.Error(message = e.localizedMessage ?: "An unexpected error occurred", data = null))
         } catch (e: IOException) {
-            emit(Resource.Error(message = "Couldn't reach server, check your internet connection"))
+            emit(Resource.Error(message = "Couldn't reach server, check your internet connection", data = null))
         }
     }
 }
