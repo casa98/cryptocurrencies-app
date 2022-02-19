2f6d88198b92f25ab8fc1a42c7a3855d80058a72
Carlos Sánchez <cagudeloa@uninorte.edu.co>
2022-02-18 22:47:27 -0500

    Complete UI and display it coin details

--- app/src/main/java/com/casa98/currencies/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/casa98/currencies/presentation/ui/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.casa98.currencies.presentation.Screen
+import com.casa98.currencies.presentation.coin_detail.CoinDetailScreen
 import com.casa98.currencies.presentation.coin_list.CoinListScreen
 import com.casa98.currencies.presentation.ui.theme.CurrenciesTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,7 +31,21 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     val navController = rememberNavController()
-                    CoinListScreen(navController = navController)
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route,
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route,
+                        ) {
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(
+                            route = "${Screen.CoinDetailScreen.route}/{coinId}",
+                        ) {
+                            CoinDetailScreen(navController = navController)
+                        }
+                    }
                 }
             }
         }
